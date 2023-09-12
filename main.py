a2ad40d1e43dfc5401e94e47cfbc17137951957d
SaltyDot <lutsenko61@gmail.com>
2023-09-12 23:23:56 +0200

    added 2st task

--- main.py
+++ main.py
@@ -19,24 +19,59 @@
 # except Exception as error:
 #     print(f"Error: {error}")
 
-try:
-    users_day = int(input("Enter the day of the week as a number: "))
-    if users_day==1:
-        print("Today is Monday")
-    elif users_day==2:
-        print("Today is Tuesday")
-    elif users_day==3:
-        print("Today is Wednesday")
-    elif users_day==4:
-        print("Today is Thursday")
-    elif users_day==5:
-        print("Today is Friday")
-    elif users_day==6:
-        print("Today is Saturday")
-    elif users_day==7:
-        print("Today is Sunday")
-    else:
-        raise Exception("Please enter a number from 1 to 7!")
-except Exception as error:
-    print(f"Error: {error}")
+# try:
+#     users_day = int(input("Enter the day of the week as a number: "))
+#     if users_day==1:
+#         print("Today is Monday")
+#     elif users_day==2:
+#         print("Today is Tuesday")
+#     elif users_day==3:
+#         print("Today is Wednesday")
+#     elif users_day==4:
+#         print("Today is Thursday")
+#     elif users_day==5:
+#         print("Today is Friday")
+#     elif users_day==6:
+#         print("Today is Saturday")
+#     elif users_day==7:
+#         print("Today is Sunday")
+#     else:
+#         raise Exception("Please enter a number from 1 to 7!")
+# except Exception as error:
+#     print(f"Error: {error}")
 
+#2nd task
+try:
+    #1st attempt
+    # number1, number2 = input("Enter two numbers:"), input()
+    # if number1.strip().isdigit() is False:
+    #     raise Exception("First entry was not a number")
+    # elif number2.strip().isdigit() is False:
+    #     raise Exception("Second entry was not a number")
+    #     не працює з від'ємними значеннями, не реалізував :(
+    number1, number2 = int(input("Enter two numbers:")), int(input())
+    if number1!=number2:
+        # метод без мин\макс
+        # if (number1)<=0 and (number2)<0:
+        #     if (number1/number2)<=0:
+        #         print(number2, number1)
+        #     else:
+        #         print(number1, number2)
+        # elif number1<0 and number2==0:
+        #     print(number1,number2)
+        # elif number1>0 and number2<=0:
+        #     print(number2, number1)
+        # elif number2>0 and number1<=0:
+        #     print(number1, number2)
+        # elif number1>=0 and number2>=0:
+        #     if number1-number2>0:
+        #         print(number2,number1)
+        #     else:
+        #         print(number1,number2)
+        print(min(number1,number2), " ", max(number1,number2))
+        # а можно лист.сорт сделать если больше 2-х значений
+    else:
+        print(number1,number2) # вывод в случае одинаковых значений
+
+except Exception as e:
+    print(f"Error:{e}")
