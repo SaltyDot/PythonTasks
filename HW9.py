e47b3438b22dbc9197521a226c841cc37f8a226f
SaltyDot <lutsenko61@gmail.com>
2023-10-06 18:38:40 +0200

    task3 attempt

--- HW9.py
+++ HW9.py
@@ -4,7 +4,6 @@
 #                     "them? To die: to sleep; No more; and by a sleep to say we end The heart-ache and the thousand "
 #                     "natural shocks That flesh is heir to, 'tis a consummation Devoutly to be wish'd. To die, to sleep"
 #                     )
-import copy
 with open("homework.txt", "w") as test_file:
     test_file.write(
         "To be, or not to be, that is the question, Whether 'tis nobler in the mind to suffer The slings and arrows "
@@ -15,7 +14,7 @@ with open("homework.txt", "w") as test_file:
 
 with open("homework.txt", "r") as test_file:
     textcopy=test_file.read().rstrip("\n")
-print(textcopy.split())
+# print(textcopy.split())
 
 punctuation='''!()-[]{};:'"\,<>./?@#$%^&*_~'''
 
@@ -23,14 +22,6 @@ for element in textcopy:
     if element in punctuation:
         textcopy=textcopy.replace(element,"")
 textcopy=textcopy.split()
-
-# # print(textcopy)
-#
-# Word_list=textcopy.split()
-# # print(Word_list)
-# for i in range (len(Word_list)):
-#     for lenght in range()
-# print(textcopy)
 
 Word_length_list=[len(i) for i in textcopy]
 # print(Word_length_list)
@@ -45,8 +36,10 @@ word_count=len(textcopy)
 print(word_count)
 
 with open("homework.txt","r") as test_file:
-    data=test_file.read()
-    data=data.replace("die","***")
-print(data)
+    newtext=test_file.read()
+    replace_count=newtext.count("die")
+    newtext=newtext.replace("die","***")
+
+print(f"Count of replaced words: {replace_count}\nNew text: {newtext}")
 
 
